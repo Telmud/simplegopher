012f2bc117b061e95c99da1c480b6814a50e35b2
Telmud <90727364+Telmud@users.noreply.github.com>
2021-11-16 16:51:20 +0100

    Initial commit

---
+++ gopherproto.py
@@ -0,0 +1,6 @@
+import asyncio
+
+class GopherProtocol(asyncio.Protocol):
+	"""Implements a simple gopher asynchronous server"""
+	def __init__(self):
+		pass
---
+++ main.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+import asyncio
+from gopherproto import GopherProtocol
+
+def main():
+    pass
+
+if __name__ == '__main__':
+	main()
